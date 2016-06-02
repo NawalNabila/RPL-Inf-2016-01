57ae68302ffd4ea7dc277cb3c60e21515cf338e8
Ira Ramadani <vierra.ramadani13@yahoo.com>
2016-06-02 22:36:27 +0700

    Page Bantuan

---
+++ Bantuan/bantuan.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile Admin</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      body {
+        background-color: #e9ece3;
+      }
+    </style>
+  </head>
+
+  <body>
+  <!--Awal konten-->
+<div class="container">
+      <div class="row">
+      <div class="col-md-7" align="center">
+          <h3 class="fixed"> Umum </h3>
+           <br>
+      </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+   
+   
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><center>Tentang Kami</center></h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="https://photos-4.dropbox.com/t/2/AACIq-vdmIjm_L8NH78S4PlkijdWn66OrARJXDSLGy5d1Q/12/346801385/jpeg/32x32/1/_/1/2/Untitled-1.jpg/EJLOuqsEGJ0FIAIoAg/8AFmol9T_cNkYU9AbNrlKBq_6RyBhMT2nizu1LiHEVw?size=1280x960&size_mode=3" class="img-circle img-responsive"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                          <td> <p>E-Tiket merupakan suatu aplikasi yang membantu para calon penumpang L300 untuk mendapatkan 
+                          tiket yang dibutuhkan tanpa harus keloket terlebih dahulu, sehingga ini akan memudahkan para calon penumpang.
+                          </p>
+                          </td>
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+                
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
+
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><center>Kontak</center></h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ5PpDr9kHlAEcdXsK5Ys72vlmHGatfrQP6S09I7s7LIenCxwPL" class="img-circle img-responsive"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                          <td> <p>Anda dapat menghubungi kami di nomor telepon 021 - 3000 7777 untuk respon cepat. Selain itu, kami juga dapat dihubungi di saluran berikut:
+
+                          <br>email : cs@E-Tiket.com
+                          <br>Pin BB : 59B3C86C
+                          <br>Line : E-Tiket
+                          <br>WhatsApp : +62 812 1239 7777
+                          </p>
+                          </td>
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+                
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
+          <br>              
+          <h3 > L300 </h3>       
+          <br>
+<div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><center>Pencarian L300</center></h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="http://shakthydoss.com/wp-content/uploads/2013/08/cartoon_detective1.png" class="img-circle img-responsive"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                          <td> <p> Anda dapat melakukan pencarian L300 dengan mengetikkan kota asal dan kota tujuan, tanggal keberangkatan dan jumlah tiket yang akan dipesan kemudian klik 'Cek Tiket' atau dapat juga mengikuti petunjuk yang ada di link berikut.
+                          </p>
+                          </td>
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+                
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
+     
+
+                    <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><center>Harga</center></h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHtj9pHX4RPeNLPWx2O2pj0y_TDU1dRROlxzXZa_cG1GiROwUjRA" class="img-circle img-responsive"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                          <td> <p> Harga yang tertera belum termasuk biaya pajak dari hotel, sedangkan untuk biaya pelayanan adalah GRATIS. Kami tidak membebankan biaya layanan kepada pelanggan. Harga akan muncul apabila Anda telah meng-klik "PESAN". 
+                          </p>
+                          </td>
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+                
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
+
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><center>Proses Booking</center></h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="http://www.argolis-yacht.com/main_images/general_photos/Booking-button.gif" class="img-circle img-responsive"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                          <td> <p> Anda dapat memesan tiket L300 di E-Tiket seperti pada panduan Cara Pemesanan. Anda dapat menghubungi customer service kami di 021 3000 7777 atau email cs@E-Tiket.com.
+                          </p>
+                          </td>
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+                
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
+
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title"><center>Pembayaran</center></h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="http://static4.depositphotos.com/1025674/382/v/950/depositphotos_3829872-Payment.jpg" class="img-circle img-responsive"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                          <td> <p> Kami dapat menerima pembayaran lewat berbagai cara, mulai dari Kartu Kredit, transfer melalui ATM, Paypal, Bayar di tempat, internet Banking, maupun melalui swalayan Indomaret.
+                          </p>
+                          </td>
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+                
+                </div>
+              </div>
+            </div>
+                 
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- AKHIR CONTENT-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
